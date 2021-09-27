26b716720954442b35b4c332d9ebdcd2be41a1b3
Hikmahx <hikmayousuph@gmail.com>
2021-09-27 12:56:27 +0100

    font-family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily:{
+      Alata:["Alata, sans-serif"], 
+      JosefinSans: ["Josefin Sans, sans-serif"]
+    }
   },
   variants: {
     extend: {},
